9b5154dfe12c8d0cd5ac958baa8adac4fdd3ba66
rizzohou <aidenhou@yeah.net>
2026-01-24 11:13:28 -0500

    feat: update paths and adjust CPU usage for tokenizer training
    - Add base path variable to centralize file path construction
    - Adjust CPU process count from `proc_num - 2` to `proc_num - 1`
    - Update dataset and model save paths to use the new base variable

--- cs336_basics/owt_tokenizer_training.py
+++ cs336_basics/owt_tokenizer_training.py
@@ -1,19 +1,20 @@
 from bpe_tokenizer_training import BPETokenizerTraining
 import os
 
-dataset_path = "data/lfs-data/owt_train.txt"
+base = "/home/hyz/projects/stanford-cs336-assignment1-basics/"
+dataset_path = base + "data/lfs-data/owt_train.txt"
 special_tokens = ["<|endoftext|>"]
 vocab_size = 32000
 proc_num = os.cpu_count()
 assert proc_num is not None
-proc_num -= 2
+proc_num -= 1
 # start bpe tokenizer training
 tokenizer_training = BPETokenizerTraining(
     dataset_path, vocab_size, special_tokens, True, proc_num
 )
 tokenizer_training.run()
 tokenizer_training.save(
-    "./models/tokenizers/owt_train-vocab.json",
-    "./models/tokenizers/owt_train-merges.txt"
+    base + "models/tokenizers/owt_train-vocab.json",
+    base + "models/tokenizers/owt_train-merges.txt"
 )
 tokenizer_training.logger.info("Tokenizer training completed.")
