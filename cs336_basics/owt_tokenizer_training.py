d6763105cc6c194d248f0173570cd2c12116cf26
rizzohou <aidenhou@yeah.net>
2026-01-24 07:09:11 -0500

    feat: add script for training BPE tokenizer on OpenWebText dataset
    - Introduce new script `owt_tokenizer_training.py` to train a BPE tokenizer
    - Configure training with 32,000 vocabulary size and special end-of-text token
    - Utilize available CPU cores for parallel processing (total cores minus 2)
    - Save trained tokenizer to models/tokenizers directory as JSON and merges files

---
+++ cs336_basics/owt_tokenizer_training.py
@@ -0,0 +1,18 @@
+from bpe_tokenizer_training import BPETokenizerTraining
+import os
+
+dataset_path = "data/lfs-data/owt_train.txt"
+special_tokens = ["<|endoftext|>"]
+vocab_size = 32000
+proc_num = os.cpu_count()
+assert proc_num is not None
+proc_num -= 2
+# start bpe tokenizer training
+tokenizer_training = BPETokenizerTraining(
+    dataset_path, vocab_size, special_tokens, True, proc_num
+)
+tokenizer_training.run()
+tokenizer_training.save(
+    "./models/tokenizers/owt_train-vocab.json",
+    "./models/tokenizers/owt_train-merges.txt"
+)
